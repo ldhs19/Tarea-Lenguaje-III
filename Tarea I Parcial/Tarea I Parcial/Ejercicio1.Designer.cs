83777934c582f1a5b5bc61d41ca179f09b45b3ae
hernandez1098 <hernandezsosa.lh@gmail.com>
2023-02-03 17:15:49 -0600

    Tarea 1
    Ejecicio par o impar y mayuscula, minuscula

---
+++ Tarea I Parcial/Tarea I Parcial/Ejercicio1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Tarea_I_Parcial
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Verbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numero1textBox = new System.Windows.Forms.TextBox();
+            this.Numero2textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Verbutton
+            // 
+            this.Verbutton.Location = new System.Drawing.Point(114, 156);
+            this.Verbutton.Name = "Verbutton";
+            this.Verbutton.Size = new System.Drawing.Size(130, 23);
+            this.Verbutton.TabIndex = 0;
+            this.Verbutton.Text = "Ver";
+            this.Verbutton.UseVisualStyleBackColor = true;
+            this.Verbutton.Click += new System.EventHandler(this.Verbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero 1:";
+            // 
+            // Numero1textBox
+            // 
+            this.Numero1textBox.Location = new System.Drawing.Point(104, 46);
+            this.Numero1textBox.Name = "Numero1textBox";
+            this.Numero1textBox.Size = new System.Drawing.Size(157, 20);
+            this.Numero1textBox.TabIndex = 2;
+            this.Numero1textBox.TextChanged += new System.EventHandler(this.Numero1textBox_TextChanged);
+            // 
+            // Numero2textBox
+            // 
+            this.Numero2textBox.Location = new System.Drawing.Point(104, 96);
+            this.Numero2textBox.Name = "Numero2textBox";
+            this.Numero2textBox.Size = new System.Drawing.Size(157, 20);
+            this.Numero2textBox.TabIndex = 4;
+            this.Numero2textBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Numero2textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Numero1textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Verbutton);
+            this.Name = "Ejercicio1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Verbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numero1textBox;
+        private System.Windows.Forms.TextBox Numero2textBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
